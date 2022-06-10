aeba290e508c61f68587c6cf8db3a85a1e1f26dc
64Book <booknarawich2001@gmail.com>
2022-06-10 11:52:01 +0700

    two commit

--- src/Hi.java
+++ src/Hi.java
@@ -7,6 +7,7 @@ public class Hi {
         b =10;
         sum = a+b;
         System.out.println("hello world!!!");
+        System.out.println("Narawich Kaweewon");
         System.out.println(100+5);
         System.out.println(sum);
     }
